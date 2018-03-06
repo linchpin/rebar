249265456888d7b8a58fcfae069f8008e6e8a915
Maxwell Morgan <mmorgan@linchpin.agency>
2018-03-06 10:34:33 -0500

    Add versioning auto-bump

--- root/theme/gulpfile.js
+++ root/theme/gulpfile.js
@@ -6,7 +6,8 @@ var gulp         = require( 'gulp' ),
 	imagemin     = require( 'gulp-imagemin' ),
 	pngquant     = require( 'imagemin-pngquant' ),
 	watch        = require( 'gulp-watch' ),
-	concat       = require( 'gulp-concat' );
+	concat       = require( 'gulp-concat' ),
+	bump         = require( 'gulp-bump' );
 
 // Uglify JS
 gulp.task( 'scripts', function() {
@@ -65,5 +66,54 @@ gulp.task( 'watch', ['sass'], function() {
 	gulp.watch('./assets/js/**/*.js', ['scripts']);
 } );
 
+gulp.task('bump:theme_version', function(type){
+    var constant = "{%= prefix_caps %}VERSION";
+    var pattern = [
+            // Match Key, e.g. "key", " OR 'key', '
+            '([\'|\"]?(',
+            constant,
+            ')[\'|\"]?[ ]*?,[ ]*[\'|\"]?)',
+
+            // Match Semver version identifier, e.g.: x.y.z
+            '(\\d+\\.\\d+\\.\\d+)',
+
+            // Match Semver pre-release identifier, e.g. -pre.0-1
+            '(-[0-9A-Za-z\.-]+)?',
+
+            // Match Semver metadata identifier, e.g. +meta.0-1
+            '(\\+[0-9A-Za-z\.-]+)?',
+
+            // Match end of version value: e.g. ", '
+            '([\'|\"]?)'
+        ].join('');
+
+    gulp.src('./functions.php')
+        .pipe(bump({
+            regex: new RegExp( pattern, "gi" ),
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+// Bump Style.css
+gulp.task( 'bump:style_css_version', function(){
+    return gulp.src('./style.css')
+        .pipe(bump({key: "Version"}))
+        .pipe(gulp.dest('.'));
+});
+
+// Bump Package.json
+gulp.task('bump:package_json_version', function(){
+    gulp.src('./package.json')
+        .pipe(bump({key: "version"}))
+        .pipe(gulp.dest('.'));
+});
+
 // Default "gulp" task
-gulp.task('default', ['scripts', 'sass', 'imagemin', 'autoprefixer', 'watch']);
+gulp.task('default', ['scripts', 'sass', 'autoprefixer', 'watch']);
+
+// Bump Version Number
+gulp.task('build:bump', ['bump:theme_version', 'bump:package_json_version', 'bump:style_css_version']);
+
+// Build out production
+// Same as default with out "watching"
+gulp.task('build:production', ['scripts', 'sass', 'imagemin', 'autoprefixer']);
